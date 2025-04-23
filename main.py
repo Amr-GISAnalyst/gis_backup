a4bcfa29688f3079ec366568cd93573293c24489
Amr <amr_abdelhameed@outlook.com>
2025-04-23 14:02:16 +0200

    Backup for the datasets and pending the featureclasses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from datetime import date
 import os
 import arcpy
+import arcpy.management
 
 DATABASE = "D:\\projects\\Floyed\\gis.sde"
 arcpy.env.overwriteOutput = True
@@ -8,29 +9,26 @@ arcpy.env.workspace = DATABASE
 
 # listing datasets and featureclasses
 datasets = arcpy.ListDatasets()
-featureclasses = arcpy.ListFeatureClasses()
-
-elements = []
-
-for fc in featureclasses:
-  elements.append(fc)
-for set in datasets:
-  elements.append(set)  
+layers = arcpy.ListFeatureClasses()
 
 # today's date with format 'dd-mm-yy'
-today = date.today().strftime('%d-%m-%Y')
+today = date.today().strftime("%d-%m-%Y")
 
 dest = "D:\\gisBackup"
 geo_name = f"{today}.gdb"
 
-#creating new geodatabase
+# creating new geodatabase
 arcpy.management.CreateFileGDB(dest, geo_name)
+
 
 print(f"new geodatabse named {geo_name} has been created")
 
-print("copying featureclasses")
+print("copying datasets ...")
+for data_set in datasets:
+  arcpy.Copy_management(f"D:\\projects\\Floyed\\gis.sde\\{data_set}", f"{dest}\\{geo_name}\\{data_set[4:]}")
+  print(f"{data_set} dataset is copied!")
 
-for element in elements:
-  arcpy.management.Copy(f'{DATABASE}\\{element}', f"{dest}\\{geo_name}")
-
-print('finished copying featureclasses and datasets')
+# print("copying featureclasses ...")
+# for layer in layers:
+#   arcpy.Copy_management(f"D:\\projects\\Floyed\\gis.sde\\{layer}", f"{dest}\\{geo_name}\\{layer[4:]}", "FeatureClass")
+#   print(f"{layer} copied!")

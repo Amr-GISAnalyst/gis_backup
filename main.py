702b72f82883c07adccf41b7edcd491e1986312e
Amr <amr_abdelhameed@outlook.com>
2025-04-25 15:53:37 +0300

    Added error handler & zip file for the geodatabase after testing will delete the gdb and then only leave zip file one more step to go but first will be testing the script.

--- main.py
+++ main.py
@@ -2,33 +2,63 @@ from datetime import date
 import os
 import arcpy
 import arcpy.management
+import shutil
+import sys
 
 DATABASE = "D:\\projects\\Floyed\\gis.sde"
 arcpy.env.overwriteOutput = True
 arcpy.env.workspace = DATABASE
 
 # listing datasets and featureclasses
+print("listing datasets and featureclasses\n---------------")
 datasets = arcpy.ListDatasets()
 layers = arcpy.ListFeatureClasses()
 
 # today's date with format 'dd-mm-yy'
+print("today's date with format 'dd-mm-yy'\n---------------")
 today = date.today().strftime("%d-%m-%Y")
 
 dest = "D:\\gisBackup"
 geo_name = f"{today}.gdb"
 
+def backup():
 # creating new geodatabase
-arcpy.management.CreateFileGDB(dest, geo_name)
+  print("creating new geodatabase\n---------------")
+  arcpy.management.CreateFileGDB(dest, geo_name)
+
+  print(f"new geodatabse named {geo_name} has been created\n---------------")
+
+  print("copying datasets ...\n---------------")
+  for data_set in datasets:
+    arcpy.Copy_management(f"D:\\projects\\Floyed\\gis.sde\\{data_set}", f"{dest}\\{geo_name}\\{data_set[4:]}")
+    print(f"{data_set} dataset is copied!")
+
+  print("All Datasets are copied! ...\n---------------")
+
+  print("copying featureclasses ...\n---------------")
+  for layer in layers:
+    arcpy.management.Copy(f"{DATABASE}\\{layer}", f"{dest}\\{geo_name}\\{layer[4:]}")
+    print(f"{layer} dataset is copied!")
+
+  print("All Featureclasses are copied! ...\n---------------")
+
+  # zipping .gdb file.
+  print("zipping geodatabase backup\n---------------")
+  shutil.make_archive(f"{dest}\\{today}", 'zip', f"{dest}\\{geo_name}")
+
+def retry(function):
+
+  for i in range(5):
+    try:
+      function()
+      break
+    except:
+      e = sys.exc_info()[1]
+      print(e.args[0])
+      shutil.rmtree(f"{dest}\\{geo_name}")
+      continue
+
+  print(f"You are all set and your GIS backup is at {dest}")
 
 
-print(f"new geodatabse named {geo_name} has been created")
-
-print("copying datasets ...")
-for data_set in datasets:
-  arcpy.Copy_management(f"D:\\projects\\Floyed\\gis.sde\\{data_set}", f"{dest}\\{geo_name}\\{data_set[4:]}")
-  print(f"{data_set} dataset is copied!")
-
-# print("copying featureclasses ...")
-# for layer in layers:
-#   arcpy.Copy_management(f"D:\\projects\\Floyed\\gis.sde\\{layer}", f"{dest}\\{geo_name}\\{layer[4:]}", "FeatureClass")
-#   print(f"{layer} copied!")
+retry(backup)
